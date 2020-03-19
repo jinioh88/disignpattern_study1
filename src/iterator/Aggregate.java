389b087e357db42cdbf288e0bd42d07f4ad87cb5
jinio <jinioh88@gmail.com>
2020-03-19 21:02:41 +0900

    add: Iterator 패턴

---
+++ src/iterator/Aggregate.java
@@ -0,0 +1,8 @@
+package iterator;
+
+import java.util.Iterator;
+
+public interface Aggregate {
+    // 집합체에 대응하는 Iterator를 작성하기 위함.
+    Iterator iterator();
+}
---
+++ src/iterator/Iterator.java
@@ -0,0 +1,8 @@
+package iterator;
+
+public interface Iterator {
+    // 종료조건 함수
+    boolean hasNext();
+
+    Object next();
+}
