c4d73a8be1e135a4697eb818a5b34bca88050553
jinio <jinioh88@gmail.com>
2020-04-04 08:27:12 +0900

    facade 패턴

---
+++ src/facade/PageMaker.java
@@ -0,0 +1,21 @@
+package facade;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PageMaker {
+    private PageMaker() {}
+
+    public static void makeWelcomePage(String mailaddr, String filename) {
+        try {
+            Properties mailprop = Database.getProperties("maildata");
+            String username = mailprop.getProperty(mailaddr);
+            HtmlWriter writer = new HtmlWriter(new FileWriter(filename));
+            writer.title("welcome " + username);
+            writer.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
